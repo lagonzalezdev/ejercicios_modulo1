123048db455993f654e8db19d0e8c08722a50442
#lagonzalezdev <gonzalezortizlau@gmail.com>
2022-10-21 15:49:51 -0500

    ejercicio

---
+++ Moises.py
@@ -0,0 +1,21 @@
+#Variables
+#snake_case
+numero = 4
+texto = "Saludos a todos"
+lista = [1,"hola", 3.4]
+#operadores logicos
+#Condicionales
+if(numero == 3):
+    print("Hola")
+elif(numero < 3):
+    print("lo que sea")
+else:
+    print("Lo siento")    
+
+#ciclos
+for elemento in lista:
+    print ("Ya casi hay pausa activa")
+
+while(numero > 0):
+    print ("Cerca del descanso")
+    numero = numero -1 
