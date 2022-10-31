4af53c5d52d65e8c28975196cabad344d6f48bd8
laugonzalezo <gonzalezortizlau@gmail.com>
2022-10-30 22:13:23 -0500

    Update isla.py
    1 cambio isla del tesoro

--- isla.py
+++ isla.py
@@ -1,13 +1,23 @@
+#algoritmo isla del tesoro - lagonzalezdev
 print("Bienvenido a la isla, tu misión será encontrar el tesoro")
 print("Puedes ir acompañado de una mascota")
 companero = input("¿deseas un compañero para tu viaje? responda si o no")
-
 if companero == "si":
-    animal=input("¿ que animal desea gato o loro?")
+    animal=input("¿qué animal desea gato o loro?")
     if animal == "loro":
-        print("el loro salio volando ahora estas perdido")
+        print("el loro salió volando ahora estas perdido")
     elif animal == "gato":
-        print("ho no es hora de la siesta dle gato pierdes un turno ")
+        print("oh no! Es hora de la siesta del gato pierdes un turno")
+    import time
+    time.sleep (2)
+    print ("el gato ha despertado, es hora de continuar la busqueda")
+decision=input("que camino tomar, ¿derecha o izquierda?")
+if decision == "derecha":
+    print ("oh no! Caiste en arenas movedizas")
+    print ("Game over")
+elif decision == "izquierda":
+    print ("Encontraste el tesoro")
+    print ("Haz ganado!!")
 elif companero == "no":
     catalejo=input("quieres buscar con el catalejo?")
     if catalejo == "si":
